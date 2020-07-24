3593dd1aa11a3d98e9edfb0ca56c3e302f335ea5
Kerambit <liukang1488666228@gmail.com>
2020-07-24 20:07:39 +0300

    Начато проектирование бд

---
+++ database/factories/MarksFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Marks;
+use Faker\Generator as Faker;
+
+$factory->define(Marks::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SubjectsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Subjects;
+use Faker\Generator as Faker;
+
+$factory->define(Subjects::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
